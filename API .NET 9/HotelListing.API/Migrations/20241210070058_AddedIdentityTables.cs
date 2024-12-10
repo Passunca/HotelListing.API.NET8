3659514d52abb4e5fd67c6ede64f2e70d3d94efc
Pedro <pedroassuncao96@gmail.com>
2024-12-10 07:07:15 +0000

    work in progress

---
+++ API .NET 9/HotelListing.API/Migrations/20241210070058_AddedIdentityTables.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedIdentityTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "24605bf6-4882-419b-b560-15fe5857fae9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f4bef5a3-bd58-47c9-8ebd-52a80fda7432");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "24605bf6-4882-419b-b560-15fe5857fae9", null, "User", "USER" },
+                    { "f4bef5a3-bd58-47c9-8ebd-52a80fda7432", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+    }
+}
